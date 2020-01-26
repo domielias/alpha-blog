048874fdf1e1beac454b9383734a7bb4076b7190
domielias <45079270+domielias@users.noreply.github.com>
2020-01-26 19:25:33 -0400

    Stlye articles listing page

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article <ApplicationRecord
   validates :title, presence: true, length:{minimum: 3, maximum: 20 }
-  validates :description, presence: true, length:{minimum: 10, maximum: 80 }
+  validates :description, presence: true, length:{minimum: 10, maximum: 500 }
 end 
